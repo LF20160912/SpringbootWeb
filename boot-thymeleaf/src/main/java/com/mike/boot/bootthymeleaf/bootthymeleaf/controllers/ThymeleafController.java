1b5f0ccda1f15bf9e8667cc602ff044016e0e448
Michael <2576731178@qq.com>
2019-07-16 00:55:06 +0800

    boot集成Thymeleaf

---
+++ boot-thymeleaf/src/main/java/com/mike/boot/bootthymeleaf/bootthymeleaf/controllers/ThymeleafController.java
@@ -0,0 +1,27 @@
+package com.mike.boot.bootthymeleaf.bootthymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class ThymeleafController {
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/data")
+    public String data(Model model) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "Mike");
+        map.put("age", 30);
+        map.put("birth", (new Date()).toString());
+        model.addAttribute("user", map);
+        return "index";
+    }
+}
