52138c36a3feec9ba417709e2f6d035252bcbdf9
Michael <2576731178@qq.com>
2019-07-16 00:29:38 +0800

    boot集成Freemarker

--- boot-freemarker/src/main/java/com/mike/boot/freemarker/bootfreemarker/controllers/FreemarkerController.java
+++ boot-freemarker/src/main/java/com/mike/boot/freemarker/bootfreemarker/controllers/FreemarkerController.java
@@ -1,12 +1,27 @@
 package com.mike.boot.freemarker.bootfreemarker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FreemarkerController {
     @GetMapping("/index")
     public String index() {
+        return "index";
+    }
+
+    @GetMapping("/data")
+    public String data(Model model) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "Mike");
+        map.put("age", 30);
+        map.put("birth", (new Date()).toString());
+        model.addAttribute("user", map);
         return "index";
     }
 }
--- boot-freemarker/src/main/resources/templates/index.ftl
+++ boot-freemarker/src/main/resources/templates/index.ftl
@@ -4,6 +4,9 @@
     <title>freemarker测试</title>
 </head>
 <body>
-boot集成freemarker
+boot集成freemarker</br>
+姓名：${user.name}</br>
+年龄：${user.age}</br>
+生日：${user.birth}</br>
 </body>
 </html>
